2282ab7655881e08cb9f61d8a0a69ac24940beff
Nishi <nishi@nishi.boats>
2024-09-18 10:08:41 +0000

    fix linux build
    git-svn-id: file:///raid/svn-personal/tewi/trunk@47 8739d7e6-ffea-ec47-b151-bdff447c6205

--- Platform/linux.mk
+++ Platform/linux.mk
@@ -2,7 +2,7 @@
 
 CC = cc
 AR = ar
-CFLAGS = -g -std=c99 -DPREFIX=\"$(PREFIX)\" -I $(PWD)/Common -D_DEFAULT_SOURCE -fPIC
+CFLAGS = -g -std=c99 -DPREFIX=\"$(PREFIX)\" -I $(PWD)/Common -D_DEFAULT_SOURCE -fPIC -D_XOPEN_SOURCE=600
 LDFLAGS =
 LIBS =
 EXEC =
