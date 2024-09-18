791c4232861dc97f021aed2172107c543cdad017
Nishi <nishi@nishi.boats>
2024-09-18 10:14:25 +0000

    add resources
    git-svn-id: file:///raid/svn-personal/tewi/trunk@48 8739d7e6-ffea-ec47-b151-bdff447c6205

--- Platform/win32.mk
+++ Platform/win32.mk
@@ -4,8 +4,10 @@ PREFIX = C:/Tewi
 
 CC = i686-w64-mingw32-gcc
 AR = i686-w64-mingw32-ar
+WINDRES = i686-w64-mingw32-windres
 CFLAGS = -g -std=c99 -DPREFIX=\"$(PREFIX)\" -I $(PWD)/Common -fPIC
 LDFLAGS =
 LIBS = -lws2_32
 EXEC = .exe
 LIB = .dll
+PREOBJS = tewi.res
--- Platform/win64.mk
+++ Platform/win64.mk
@@ -4,8 +4,10 @@ PREFIX = C:/Tewi
 
 CC = x86_64-w64-mingw32-gcc
 AR = x86_64-w64-mingw32-ar
+WINDRES = x86_64-w64-mingw32-windres
 CFLAGS = -g -std=c99 -DPREFIX=\"$(PREFIX)\" -I $(PWD)/Common -fPIC
 LDFLAGS =
 LIBS = -lws2_32
 EXEC = .exe
 LIB = .dll
+PREOBJS = tewi.res
