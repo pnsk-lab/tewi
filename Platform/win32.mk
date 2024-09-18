ce7c8c96bc1b1021cedc003a303f474518d02294
Nishi <nishi@nishi.boats>
2024-09-18 08:46:50 +0000

    add win32.mk
    git-svn-id: file:///raid/svn-personal/tewi/trunk@42 8739d7e6-ffea-ec47-b151-bdff447c6205

--- Platform/win32.mk
+++ Platform/win32.mk
@@ -2,10 +2,10 @@
 
 PREFIX = C:/Tewi
 
-CC = x86_64-w64-mingw32-gcc
-AR = x86_64-w64-mingw32-ar
+CC = i686-w64-mingw32-gcc
+AR = i686-w64-mingw32-ar
 CFLAGS = -g -std=c99 -DPREFIX=\"$(PREFIX)\" -I $(PWD)/Common -I $(PWD)/openssl/include -fPIC
-LDFLAGS = -L $(PWD)/openssl/lib64
+LDFLAGS = -L $(PWD)/openssl/lib32
 LIBS = -lws2_32
 EXEC = .exe
 LIB = .dll
