8358d2fafd8ebbf55167b32ac6ea7343232453c3
Nishi <nishi@nishi.boats>
2024-09-19 11:11:55 +0000

    installer
    git-svn-id: file:///raid/svn-personal/tewi/trunk@79 8739d7e6-ffea-ec47-b151-bdff447c6205

--- installer.sh
+++ installer.sh
@@ -4,10 +4,13 @@
 fail() {
 	rm -f tewi-service.exe
 	rm -f tewi.exe
+	mv config.h.bak config.h
 	exit 1
 }
 
 VERSION=`make get-version`
+
+sed "s/undef NO_SSL/define NO_SSL/g" config.h.tmpl > config.h
 
 make clean || fail
 make PLATFORM=$1 -j4 || fail
@@ -17,5 +20,20 @@ make PLATFORM=$1-service -j4 || fail
 cp Server/tewi.exe tewi-service.exe
 cd Server
 makensis -DVERSION=$VERSION install.nsi
+cp install.exe ../install-nossl.exe
+rm -f tewi.exe tewi-service.exe
+cd ..
+
+sed "s/define NO_SSL/undef NO_SSL/g" config.h.tmpl > config.h
+
+make clean || fail
+make PLATFORM=$1 -j4 || fail
+cp Server/tewi.exe tewi.exe
+make clean || fail
+make PLATFORM=$1-service -j4 || fail
+cp Server/tewi.exe tewi-service.exe
+cd Server
+makensis -DVERSION=$VERSION install.nsi
+cp install.exe ../install-ssl.exe
 rm -f tewi.exe tewi-service.exe
 cd ..
