d4f11bc079381eaaab74322683fb6fc47978ef54
Nishi <nishi@nishi.boats>
2024-09-24 19:44:49 +0000

    fix macro
    git-svn-id: file:///raid/svn-personal/tewi/trunk@149 8739d7e6-ffea-ec47-b151-bdff447c6205

--- Platform/win64-service.mk
+++ Platform/win64-service.mk
@@ -5,7 +5,7 @@ PREFIX = C:/Tewi
 CC = x86_64-w64-mingw32-gcc
 AR = x86_64-w64-mingw32-ar
 WINDRES = x86_64-w64-mingw32-windres
-CFLAGS = -g -std=c99 -DPREFIX=\"$(PREFIX)\" -I $(PWD)/Common -fPIC -mwindows
+CFLAGS = -g -std=c99 -DPREFIX=\"$(PREFIX)\" -I $(PWD)/Common -fPIC -DSERVICE -mwindows
 LDFLAGS = -mwindows
 LIBS = -lws2_32
 EXEC = .exe
