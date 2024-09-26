98f836335840e5db035e0dd01af5e6eec9b293d9
Nishi <nishi@nishi.boats>
2024-09-26 20:45:59 +0000

    some cygwin archive creator
    git-svn-id: file:///raid/svn-personal/tewi/trunk@172 8739d7e6-ffea-ec47-b151-bdff447c6205

--- cygwin.sh
+++ cygwin.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 # $Id$
+rm -rf tewidist
 make PREFIX=/cygdrive/c/Tewi DESTDIR=tewidist PLATFORM=cygwin install
-cp /usr/bin/cygwin1.dll tewidist/
+cp -rfv tewidist/cygdrive/c/Tewi tewidist/Tewi
+cp /usr/bin/cygwin1.dll tewidist/Tewi/bin/
+rm -rf tewidist/cygdrive
+echo "@echo off" > tewidist/install.bat
+echo "echo This installer will attempt to overwrite files." >> tewidist/install.bat
+echo "echo Make sure you make backups." >> tewidist/install.bat
+echo "pause" >> tewidist/install.bat
+echo "xcopy /e Tewi C:\\Tewi" >> tewidist/install.bat
+echo "echo Tewi HTTPd has been installed." >> tewidist/install.bat
+echo "pause" >> tewidist/install.bat
+echo "exit" >> tewidist/install.bat
+unix2dos tewidist/install.bat
+rm -f tewidist.zip
 zip -rv tewidist.zip tewidist
+rm -rf tewidist
