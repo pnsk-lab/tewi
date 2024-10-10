d0dd881bd132f9eb943a505817c4bb622c76478d
Nishi <nishi@nishi.boats>
2024-10-10 02:24:11 +0000

    use sane shell
    git-svn-id: file:///raid/svn-personal/tewi/trunk@308 8739d7e6-ffea-ec47-b151-bdff447c6205

--- Platform/unixware.mk
+++ Platform/unixware.mk
@@ -1,10 +1,12 @@
 # $Id$
 
+SHELL = /usr/bin/bash
+
 CC = cc
 AR = ar
 CFLAGS = -DPREFIX=\"$(PREFIX)\" -I $(PWD)/Common -I $(PWD)/VC6Compat
 LDFLAGS =
 SHARED = -G
-LIBS = -lnetwork -ldl
+LIBS = -lsocket -ldl
 EXEC =
 LIBSUF = .so
