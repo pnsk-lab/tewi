a32a74333e84ac90954e9feb1f9cd54973258e69
Nishi <nishi@nishi.boats>
2024-10-14 17:48:14 +0000

    trying to add nextstep support
    git-svn-id: file:///raid/svn-personal/tewi/trunk@326 8739d7e6-ffea-ec47-b151-bdff447c6205

--- Platform/nextstep.mk
+++ Platform/nextstep.mk
@@ -2,7 +2,7 @@
 
 CC = cc
 AR = ar
-CFLAGS = -g -DPREFIX=\"$(PREFIX)\" -I ../VC6Compat -I $(PWD)/Common -D_POSIX_SOURCE -D_XOPEN_SOURCE=600 -Duint64_t=uint32_t -DREALLY_OLD
+CFLAGS = -g -DPREFIX=\"$(PREFIX)\" -I ../VC6Compat -I $(PWD)/Common -D_POSIX_SOURCE -D_NEXT_SOURCE -Duint64_t=uint32_t -DREALLY_OLD
 LDFLAGS =
 LIBS = -ldl
 EXEC =
