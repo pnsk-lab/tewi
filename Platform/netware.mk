5bea30fbd3c4b60baca07074f6de74f3fb42630a
Nishi <nishi@nishi.boats>
2024-10-17 04:54:34 +0000

    update
    git-svn-id: file:///raid/svn-personal/tewi/trunk@373 8739d7e6-ffea-ec47-b151-bdff447c6205

--- Platform/netware.mk
+++ Platform/netware.mk
@@ -7,7 +7,7 @@ CC = owcc
 AR = wlib
 AR_FLAGS = -q -b -n -fo
 CFLAGS = -b netware_clib_lite -I $(NOVELLNDK)/include -I $(NOVELLNDK)/include/nlm -g -std=c99 -DPREFIX=\"$(PREFIX)\" -I ../Common -I ../VC6Compat -fPIC -D__WATCOM_LFN__ -Duint64_t=uint32_t -DN_PLAT_NLM
-LDFLAGS = -b netware_clib_lite
+LDFLAGS = -b netware_clib_lite -Wl,"option copyright 'Original by Nishi <nishi@nishi.boats>'" -Wl,"option description 'Tewi HTTPd'"
 MODULE =
 LIBS =
 EXEC = .nlm
