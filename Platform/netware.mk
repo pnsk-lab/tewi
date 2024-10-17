740e7548b1a933d29172c1f4708f84ae67f5bbc0
Nishi <nishi@nishi.boats>
2024-10-17 04:32:38 +0000

    remove module support
    git-svn-id: file:///raid/svn-personal/tewi/trunk@370 8739d7e6-ffea-ec47-b151-bdff447c6205

--- Platform/netware.mk
+++ Platform/netware.mk
@@ -8,6 +8,7 @@ AR = wlib
 AR_FLAGS = -q -b -n -fo
 CFLAGS = -b netware_clib_lite -I $(NOVELLNDK)/include -I $(NOVELLNDK)/include/nlm -g -std=c99 -DPREFIX=\"$(PREFIX)\" -I ../Common -I ../VC6Compat -fPIC -D__WATCOM_LFN__ -Duint64_t=uint32_t -DN_PLAT_NLM
 LDFLAGS = -b netware_clib_lite
+MODULE =
 LIBS =
 EXEC = .nlm
 STATIC = lib
